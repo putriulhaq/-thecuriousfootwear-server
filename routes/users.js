6111507913213bfbca00485c821a953cad497ee8
farishy <muhammadfaris9d@gmail.com>
2023-01-04 20:41:25 +0700

    api like and dislike comment

--- routes/users.js
+++ routes/users.js
@@ -2,42 +2,41 @@ import express from "express";
 const User = express.Router();
 import Post from "../models/post.js";
 import Users from "../models/User.js";
+import comment from "../models/comment.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 User.put("/like/:postId", authMiddleware, async (req, res) => {
-  const id = req.user.userId
-  const postId = req.params.postId
+  const id = req.user.userId;
+  const postId = req.params.postId;
   try {
     await Post.findByIdAndUpdate(postId, {
       $addToSet: { like: id },
-      $pull: { dislike: id }
-    })
-    res.status(200).json("The video has been likes")
-
+      $pull: { dislike: id },
+    });
+    res.status(200).json("The video has been likes");
   } catch (err) {
-    return (err)
+    return err;
   }
-})
+});
 
 User.put("/dislike/:postId", authMiddleware, async (req, res) => {
-  const id = req.user.userId
-  const postId = req.params.postId
+  const id = req.user.userId;
+  const postId = req.params.postId;
   try {
     await Post.findByIdAndUpdate(postId, {
       $addToSet: { dislike: id },
-      $pull: { like: id }
-    })
-    res.status(200).json("The video has been dislikes")
-
+      $pull: { like: id },
+    });
+    res.status(200).json("The video has been dislikes");
   } catch (err) {
-    return (err)
+    return err;
   }
-})
+});
 
 User.get("/profil/:Id", async (req, res) => {
   const { Id } = req.params;
-  const dataProfil = await Users.find({ userId: Id })
-  const data = dataProfil.map(data => {
+  const dataProfil = await Users.find({ userId: Id });
+  const data = dataProfil.map((data) => {
     return {
       userId: data.userId,
       first_name: data.first_name,
@@ -46,17 +45,17 @@ User.get("/profil/:Id", async (req, res) => {
       email: data.email,
       phone_number: data.phone_number,
       about: data.about,
-      image: data.image
-    }
-  })
-  res.json(data)
-})
+      image: data.image,
+    };
+  });
+  res.json(data);
+});
 
 User.put("/profil/edit/:id", async (req, res) => {
   const { id } = req.params;
   const myquery = { userId: id };
   const updateData = {
-    $set: req.body
+    $set: req.body,
   };
   const data = await Users.updateOne(
     myquery,
@@ -70,8 +69,36 @@ User.put("/profil/edit/:id", async (req, res) => {
     }
   ).clone();
   return res.status(200).send({
-    message: `Updated profile successfully`
+    message: `Updated profile successfully`,
   });
+});
+
+User.put("/likeComment/:commentId", authMiddleware, async (req, res) => {
+  const id = req.user.userId;
+  const commentId = req.params.commentId;
+  try {
+    await comment.findByIdAndUpdate(commentId, {
+      $addToSet: { like: id },
+      $pull: { dislike: id },
+    });
+    res.status(200).json("Comment has been liked");
+  } catch (err) {
+    return err;
+  }
+});
+
+User.put("/dislikeComment/:commentId", authMiddleware, async (req, res) => {
+  const id = req.user.userId;
+  const commentId = req.params.commentId;
+  try {
+    await comment.findByIdAndUpdate(commentId, {
+      $addToSet: { dislike: id },
+      $pull: { like: id },
+    });
+    res.status(200).json("Comment has been disliked");
+  } catch (err) {
+    return err;
+  }
 });
 
 export default User;
